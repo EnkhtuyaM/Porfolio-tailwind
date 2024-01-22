b7c1df7e66c854273431e89aeffe8c69a31e26a1
EnkhtuyaM <enkhtuyam1001@gmail.com>
2024-01-22 21:58:17 +0800

    from home

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,69 @@
-export const Experience = () => {
-  return (
-    <div className="w-[1440px] h-1140px] flex flex-col">
-      <div className="flex justify-center mt-12">
-        <button className="flex w-[105px] h-7 border-2 rounded-lg justify-center">
-          Experience
-        </button>
-      </div>
-      <div className="flex justify-between ">
-        <h3 className="w-[1216px] h-[28px] ps-[530px] mt-6 mb-16">
+export const Experience=()=>{
+    return(
+      <div className="w-[1440px] h-[1140px] flex flex-col bg-gray-50">
+        <div className="flex justify-center mt-12">
+          <button className="flex w-[105px] h-7 border-2 rounded-lg justify-center">Experience</button>
+        </div>
+        <div className="flex justify-between ">
+          <h3 className="w-[1216px] h-[28px] ps-[530px] mt-6 mb-12">
           Here is a quick summary of my most recent experiences:
-        </h3>
+          </h3>
+        </div>
+        <div className="w-[1440px] h-[1140px] bg-gray-50 flex flex-col gap-12"> 
+        <div className="w-[1280px] h-[288px] flex gap-[48px] pl-[164px] ml-[32px] bg-white">
+          <div className="w-[102px] h-[28px]">
+            <img src="./logo-upwork.png" alt="" />
+  
+          </div>
+          <div className="w-[384px] h-[28px] pl-14 ml-24">
+          <p className="font-bold">Sr. Frontend Developer</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Ut pretium arcu et massa semper, id fringilla leo semper.</p>
+          <p>Sed quis justo ac magna.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div>
+          <p className="w-[146px] h-[24px]">Nov 2021 - Present</p>
+        </div>
+
+        </div>
+        <div className="w-[1280px] h-[288px] flex gap-[48px] pl-[164px] ml-[32px] bg-white">
+          <div className="w-[102px] h-[28px]">
+            <img src="./logo-upwork.png" alt="" />
+  
+          </div>
+          <div className="w-[384px] h-[28px] pl-14 ml-24">
+          <p className="font-bold">Team Lead</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Ut pretium arcu et massa semper, id fringilla leo semper.</p>
+          <p>Sed quis justo ac magna.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div>
+          <p className="w-[146px] h-[24px]">Jul 2017 - Oct 2021</p>
+        </div>
+
+        </div>
+        <div className="w-[1280px] h-[288px] flex gap-[48px] pl-[164px] ml-[32px] bg-white">
+          <div className="w-[102px] h-[28px]">
+            <img src="./logo-upwork.png" alt="" />
+  
+          </div>
+          <div className="w-[384px] h-[28px] pl-14 ml-24">
+          <p className="font-bold">Full Stack Developer</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         
+        </div>
+        <div>
+          <p className="w-[146px] h-[24px]">Nov 2021 - Present</p>
+        </div>
+
+        </div>
+          
+        </div>
+
+
+  
       </div>
-    </div>
-  );
-};
+    )
+  }
